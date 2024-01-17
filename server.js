d122ea1902a63a873ea80e9213822edcdf0c446f
Jasmine Lee <jasminelee_0409@hotmail.com>
2024-01-17 01:30:15 -0500

    Added the add functionalities and the prompts for the update functionality.

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ function options() {
 }
 
 function viewAllDepartment(){
-      db.query('SELECT * FROM department', function (err, results) {
+      db.query('SELECT * FROM department;', function (err, results) {
         if (err){
             console.log("Unable to retrieve the department table.");
             return options();
@@ -63,6 +63,160 @@ function viewAllDepartment(){
         return options();
     });
 
+}
+
+function viewAllRoles(){
+    db.query('SELECT * FROM role;', function (err, results) {
+        if (err){
+            console.log("Unable to retrieve the role table.");
+            return options();
+        }
+        console.table(results);
+        return options();
+    });
+}
+
+function viewAllEmployees() {
+  db.query(
+    `SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_id
+    FROM department
+    JOIN role ON department.id = role.department_id
+    JOIN employee ON role.id = employee.role_id;`,
+    function (err, results) {
+      if (err) {
+        console.log("Unable to retrieve the employee table.");
+        return options();
+      }
+      console.table(results);
+      return options();
+    }
+  );
+}
+
+function addDepartment(){
+  inquirer
+    .prompt({
+      type: "input",
+      name: "add_department",
+      message: "Please enter the department that you would like to add.",
+    })
+    .then((response) => {
+      const departmentName = response.add_department;
+      db.query(`INSERT INTO department (name) VALUES ('${departmentName}');`, function (err, results) {
+          if (err) {
+            console.log("Unable to add the department");
+            console.log(err)
+            return options();
+          } else {
+            console.log("Successfully added the department.");
+            return options();
+          }
+        });
+    });
+}
+
+function addRole(){
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "Please enter the role that you would like to add:",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "Please enter the salary for the role:",
+      },
+      {
+        type: "input",
+        name: "department_id",
+        message: "Please enter the department ID of the role:",
+      },
+    ])
+    .then((response) => {
+      const { title, salary, department_id } = response;
+      db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${title}', ${salary}, ${department_id});`, function (err, results) {
+          if (err) {
+            console.log("Unable to add the role.");
+            console.log(err)
+            return options();
+          } else {
+            console.log("Successfully added the role!");
+            return options();
+          }
+        });
+    });
+}
+
+function addEmployee(){
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "fname",
+        message: "Please enter the employee's first name:",
+      },
+      {
+        type: "input",
+        name: "lname",
+        message: "Please enter the employee's last name:",
+      },
+      {
+        type: "input",
+        name: "role_id",
+        message: "Please enter the employee's role ID:",
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "Please enter the employee's manager ID:",
+      },
+    ])
+    .then((response) => {
+      const { fname, lname, role_id, manager_id } = response;
+      db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${fname}', '${lname}', ${role_id}, ${manager_id});`, function (err, results) {
+          if (err) {
+            console.log("Unable to add the employee.");
+            console.log(err)
+            return options();
+          } else {
+            console.log("Successfully added the employee!");
+            return options();
+          }
+        });
+    });
+}
+
+
+function updateEmployeeRole(){
+    const employeeList = db.query(`SELECT id, CONCAT(first_name, " ", last_name) AS fullname FROM employee;`);
+    inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employee_name",
+        message: "Which employee's role would you like to update?",
+        choices: [
+            employeeList
+        ]
+      },
+      {
+        type: "list",
+        name: "lname",
+        message: "Which role would you like to assign to the selected employee?",
+        choices: [
+
+        ]
+      },
+    ])
+    .then((response) => {
+
+        });
+    };
+
+function exit(){
+    console.log(`Thank you for visiting ABC company's database. See you next time!`)
 }
 
 // async function viewAllDepartment(){
@@ -75,32 +229,5 @@ function viewAllDepartment(){
 //     }
 //     options();
 // }
-
-function viewAllRoles(){
-    db.query('SELECT * FROM role', function (err, results) {
-        if (err){
-            console.log("Unable to retrieve the role table.");
-            return options();
-        }
-        console.table(results);
-        return options();
-    });
-}
-
-function viewAllEmployees(){
-    db.query('SELECT * FROM employee', function (err, results) {
-        if (err){
-            console.log("Unable to retrieve the employee table.");
-            return options();
-        }
-        console.table(results);
-        return options();
-    });
-}
-
-function exit(){
-    console.log(`Thank you for visiting ABC company's database. See you next time!`)
-}
-
 
 options();
